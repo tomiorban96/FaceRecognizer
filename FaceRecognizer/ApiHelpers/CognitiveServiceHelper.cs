106f237c54d8c894311b29e34de078109e7ba7f3
tomiorban96 <tomiorban96@gmail.com>
2019-05-06 12:51:50 +0200

    Initial commit

---
+++ FaceRecognizer/ApiHelpers/CognitiveServiceHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http.Headers;
+using Windows.Graphics.Imaging;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace FaceRecognizer.ApiHelpers
+{
+    public class CognitiveServiceHelper
+    {
+        static string uriBase = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect";
+        static string ApiKey1 = "90c5e8bc2b8043f4a09cd5c5c7ba2514";
+        static string ApiKey2 = "8e45afaa73c747f2b395b509af2a5a10";
+
+        
+
+        public static async void FetchApi(SoftwareBitmap bitmap)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ApiKey1);
+
+            var requestParameters = "returnFaceId=false";
+
+            var uri = $"{uriBase}?{requestParameters}";
+
+            HttpResponseMessage response;
+            string contentString;
+
+            var bytes = await GetImageAsByteArray(bitmap);
+
+
+            using (var content = new ByteArrayContent(bytes))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                response = await client.PostAsync(uri, content);
+
+                contentString = await response.Content.ReadAsStringAsync();
+            }
+ 
+            
+        }
+
+        private static async Task<byte[]> GetImageAsByteArray(SoftwareBitmap softwareBitmap)
+        {
+            byte[] bytes;
+            WriteableBitmap newBitmap = new WriteableBitmap(softwareBitmap.PixelWidth, softwareBitmap.PixelHeight);
+            softwareBitmap.CopyToBuffer(newBitmap.PixelBuffer);
+
+            using (var stream = newBitmap.PixelBuffer.AsStream())
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+
+
+            using (var stream = newBitmap.PixelBuffer.AsStream())
+            {
+                await stream.WriteAsync(bytes, 0, bytes.Length);
+            }
+
+            return bytes;
+        }
+    }
+}
---
+++ FaceRecognizer/MainPage.xaml.cs
@@ -0,0 +1,170 @@
+﻿using FaceRecognizer.ApiHelpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.System.Display;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Windows.Graphics;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FaceRecognizer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaCapture mediaCapture;
+        DisplayRequest displayRequest = new DisplayRequest();
+        bool isPreviewing;
+
+        
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Application.Current.Resuming += Application_Resuming;
+            Application.Current.Suspending += Application_Suspending;
+            ImagePopup.IsOpen = false;
+
+        }
+
+        private async Task StartPreviewAsync()
+        {
+            try
+            {
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync();
+
+                displayRequest.RequestActive();
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return;
+            }
+
+            try
+            {
+                PreviewControl.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+                isPreviewing = true;
+            }
+            catch (System.IO.FileLoadException)
+            {
+                //mediaCapture.CaptureDeviceExclusiveControlStatusChanged
+            }
+        }
+
+        private async Task CleanupCameraAsync()
+        {
+            if (mediaCapture != null)
+            {
+                if (isPreviewing)
+                {
+                    await mediaCapture.StopPreviewAsync();
+                }
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    PreviewControl.Source = null;
+                    if (displayRequest != null)
+                    {
+                        displayRequest.RequestRelease();
+                    }
+
+                    mediaCapture.Dispose();
+                    mediaCapture = null;
+                });
+            }
+        }
+
+        private async void Application_Suspending(object sender, SuspendingEventArgs e)
+        {
+            if (Frame.CurrentSourcePageType == typeof(MainPage))
+            {
+                var deferral = e.SuspendingOperation.GetDeferral();
+                await CleanupCameraAsync();
+                deferral.Complete();
+            }
+        }
+
+        private async void Application_Resuming(object sender, object e)
+        {
+            await StartPreviewAsync();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await StartPreviewAsync();
+        }
+
+        private async void CaptureButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Capturing the photo
+            var capture = await mediaCapture.PrepareLowLagPhotoCaptureAsync(ImageEncodingProperties.CreateUncompressed(MediaPixelFormat.Bgra8));
+            var photo = await capture.CaptureAsync();
+
+
+            //Creating the bitmap
+            var softwareBitmap = photo.Frame.SoftwareBitmap;
+
+            await capture.FinishAsync();
+            
+            //API call
+            CognitiveServiceHelper.FetchApi(softwareBitmap);
+
+            //Converting the bitmap
+            var convertedSoftwareBitmap = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+
+            //Creating a bitmap source to set to the image source
+            var bitmapSource = new SoftwareBitmapSource();
+            await bitmapSource.SetBitmapAsync(convertedSoftwareBitmap);
+
+            //Drawing the taken picture (without drawing the face rectangle on it, due to fail in the API call)
+            Photo.Source = bitmapSource;
+
+            //Setting the size of the wrapper grid element, so the popups size is the full view
+            WrapperGrid.Width = Window.Current.Bounds.Width;
+            WrapperGrid.Height = Window.Current.Bounds.Height;
+
+            //Disabling capture button
+            CaptureButton.IsEnabled = false;
+
+            //Showing the popup
+            ImagePopup.IsOpen = true;
+            
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Enabling capture button
+            CaptureButton.IsEnabled = true;
+
+            //Closing the popup
+            ImagePopup.IsOpen = false;
+        }
+
+
+    }
+}
